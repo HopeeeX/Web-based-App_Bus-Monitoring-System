397861dd81c86f275a3ffacb66d1954bb68d7bcd
ChoiMillie <christinemedalla01@gmail.com>
2023-04-08 15:26:16 +0800

    Updating Folders and Added changes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '642F84',
+        secondary: 'A7A9AC',
+        tertiary: 'E9E9E9',
+        highlight: '#c3aecf',
+      }
+    },
+      gradientColorStops: {
+        gradient1: {
+          'start': '642F84',
+          'end': '#9571ab'
+        }
+      }
   },
   plugins: [],
 }

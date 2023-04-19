eb1de5abac2e1be4cbb2f267c285d8dfcf419b16
ChoiMillie <christinemedalla01@gmail.com>
2023-04-19 21:38:32 +0800

    Added icons and img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import React from 'react'
+import LoginScreen from './pages/common/LoginScreen'
 
 function App() {
   return (
     <div>
+      <LoginScreen/>
     </div>
   );
 }
--- src/pages/common/LoginScreen.jsx
+++ src/pages/common/LoginScreen.jsx
@@ -1,2 +1,21 @@
+import React from 'react'
+import UbeLogo from '../../assets/images/UbeLogo.png'
 
 
+const LoginScreen = () => {
+    return (
+        <div className='w-full h-screen flex'>
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div className='hidden md:block'>
+                    <img src={UbeLogo} alt="logo"/>
+                </div>
+                <div>
+                    <h2>Form</h2>
+                    </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default LoginScreen;

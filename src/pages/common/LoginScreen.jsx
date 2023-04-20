6bf21034645b88ce81f37b31a73cedb50427d87b
ChoiMillie <christinemedalla01@gmail.com>
2023-04-20 23:40:08 +0800

    Finish Login Screen Frontend

--- src/pages/common/LoginScreen.jsx
+++ src/pages/common/LoginScreen.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
-import UbeLogo from '../../assets/images/UbeLogo.png'
-
+import React, { useState} from 'react';
+import UbeLogo from '../../assets/images/UbeLogo.png';
+import BusDriver from '../../assets/images/BusDriver.png';
+import User from '../../assets/icons/User.png';
+import Eye from '../../assets/icons/Eye.png';
+import EyeOff from '../../assets/icons/EyeOff.png';
 
 const LoginScreen = () => {
-    return (
-        <div className='w-full h-screen flex'>
-            <div className='grid grid-cols-1 md:grid-cols-2'>
-                <div className='hidden md:block'>
-                    <img src={UbeLogo} alt="logo"/>
-                </div>
-                <div>
-                    <h2>Form</h2>
-                    </div>
-            </div>
-        </div>
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-    )
-}
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // Do something when the login button is clicked
+    };
+
+    return (
+    <div className='w-full h-screen flex'>
+        <div className='grid grid-cols-2 m-auto shadow-lg rounded-xl overflow-hidden'style={{width:'1057px', height: '661px'}}>
+        <div className='hidden md:flex flex-col justify-center items-center'>
+            <img src={UbeLogo} alt='logo' style={{ height: '138px' }} />
+            <img src={BusDriver} alt='bus-driver' style={{ height: '437.48px', width: '400px'}} />
+        </div>
+        <div className='flex flex-col items-center justify-center bg-primary rounded-tr-xl rounded-br-xl overflow-hidden'>
+            <form className='flex flex-col items-center'>
+                <h1 className='text-white font-bold text-4xl font-work-sans text-center mb-3'>User Login</h1>
+                <p className='font-inter text-sm text-center text-purple-300'>Please login to continue</p>
+                <div className='flex flex-col '>
+                    <label className='text-white font-inter font-semibold text-sm mb-1 mt-5
+                    ' htmlFor='username'>Username</label>
+                    <div className='relative'>
+                    <input className='border outline-none border-gray-300 rounded-lg h-10 w-80 pl-4 bg-transparent text-white font-inter' type='text' id='username' placeholder='Enter your username'/>
+                    <img src={User} alt='user' className='absolute right-3 top-2' style={{height: '24px', width: '24px'}}/>
+                    </div>
+                </div>
+                <div className='flex flex-col'>
+                    <label className='text-white font-inter font-semibold text-sm mb-1 mt-5
+                    ' htmlFor='password'>Password</label>
+                    <div className='relative'>
+                    <input className='border outline-none border-gray-300 rounded-lg h-10 w-80 pl-4 bg-transparent text-white font-inter' type={isPasswordVisible ? 'text': 'password'} id='password' placeholder='Enter your password'/>
+                    <img src={isPasswordVisible ? Eye : EyeOff} alt='eye' className='absolute right-3 top-2' style={{height: '24px', width: '24px', cursor: 'pointer'}} onClick={togglePasswordVisibility}/>
+                    </div>
+                    <div className='mt-2 text-sm text-right text-purple-300'>
+                        <a href='#'>Forgot your password?</a>
+                    </div>
+                </div>
+                <button type='submit' className='bg-secondary text-white font-inter rounded-full px-4 py-2 mt-5 font-semibold hover:bg-gray-500' style={{height: '40px', width: '280px'}} onClick={handleLogin}>Login</button>
+            </form>
+        </div>
+        </div>
+    </div>
+    );
+};
 
 export default LoginScreen;

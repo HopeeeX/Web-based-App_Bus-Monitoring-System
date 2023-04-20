0df887f558d6fdfcdbea8f16dfac0bdba2e8aeac
ChoiMillie <christinemedalla01@gmail.com>
2023-04-20 23:37:48 +0800

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        primary: '642F84',
-        secondary: 'A7A9AC',
-        tertiary: 'E9E9E9',
+        primary: '#642F84',
+        secondary: '#A7A9AC',
+        tertiary: '#E9E9E9',
         highlight: '#c3aecf',
       }
     },
       gradientColorStops: {
         gradient1: {
-          'start': '642F84',
+          'start': '#642F84',
           'end': '#9571ab'
         }
       }

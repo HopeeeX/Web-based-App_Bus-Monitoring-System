26e5952137075812a8d1bf6c4b7033c105899f89
ChoiMillie <christinemedalla01@gmail.com>
2023-04-22 17:04:19 +0800

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import LoginScreen from './pages/common/LoginScreen'
+import './App.css';
+import ForgotPassword from './pages/common/ForgotPassword';
+import LoginPage from './pages/common/LoginPage';
+import { Routes, Route} from 'react-router-dom';
 
 function App() {
-
-  return (
-    <>
-    <div>
-      <LoginScreen/>
-    </div>
-    </>
+  return(
+    <Routes>
+      <Route path='/' element={<LoginPage/>}/>
+      <Route path='/forgot' element={<ForgotPassword/>}/>
+    </Routes>
   )
 }
 
-export default App
+export default App;

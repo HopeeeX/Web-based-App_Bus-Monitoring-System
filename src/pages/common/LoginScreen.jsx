b98c47f0e4c3b48f539924816225a3de682dea6b
ChoiMillie <christinemedalla01@gmail.com>
2023-04-22 17:12:48 +0800

    Added Changes

--- src/pages/common/LoginScreen.jsx
+++ src/pages/common/LoginScreen.jsx
@@ -4,7 +4,7 @@ import BusDriver from '../../assets/images/BusDriver.png';
 import User from '../../assets/icons/User.png';
 import Eye from '../../assets/icons/Eye.png';
 import EyeOff from '../../assets/icons/EyeOff.png';
-import {BrowserRouter as Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const LoginScreen = () => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false);
@@ -19,33 +19,33 @@ const LoginScreen = () => {
     };
 
     return (
-    <div className='w-full h-screen flex'>
+    <div className='w-full h-screen flex items-center'>
         <div className='grid grid-cols-2 m-auto shadow-xl rounded-xl overflow-hidden'style={{width:'1057px', height: '661px'}}>
         <div className='hidden md:flex flex-col justify-center items-center'>
             <img src={UbeLogo} alt='logo' style={{ height: '138px' }} />
             <img src={BusDriver} alt='bus-driver' style={{ height: '437.48px', width: '400px'}} />
         </div>
-        <div className='flex flex-col items-center justify-center bg-primary rounded-tr-xl rounded-br-xl overflow-hidden'>
+        <div className='flex flex-col items-center justify-center bg-primary rounded-tr-xl rounded-br-xl overflow-hidden '>
             <form className='flex flex-col items-center'>
                 <h1 className='text-white font-bold text-4xl font-work-sans text-center mb-3'>User Login</h1>
                 <p className='font-inter text-sm text-center text-purple-300'>Please login to continue</p>
-                <div className='flex flex-col '>
-                    <label className='text-white font-inter font-semibold text-sm mb-1 mt-5
+                <div className='flex flex-col items-start '>
+                    <label className='text-white font-inter font-semibold text-sm mb-1 mt-5 mr-7
                     ' htmlFor='username'>Username</label>
                     <div className='relative'>
                     <input className='border outline-none border-gray-300 rounded-lg h-10 w-80 pl-4 bg-transparent text-white font-inter' type='text' id='username' placeholder='Enter your username'/>
                     <img src={User} alt='user' className='absolute right-3 top-2' style={{height: '24px', width: '24px'}}/>
                     </div>
                 </div>
-                <div className='flex flex-col'>
-                    <label className='text-white font-inter font-semibold text-sm mb-1 mt-5
-                    ' htmlFor='password'>Password</label>
+                <div className='flex flex-col items-start'>
+                    <label className='text-white font-inter font-semibold text-sm mb-1 mt-5' 
+                        htmlFor='password'>Password</label>
                     <div className='relative'>
                     <input className='border outline-none border-gray-300 rounded-lg h-10 w-80 pl-4 bg-transparent text-white font-inter' type={isPasswordVisible ? 'text': 'password'} id='password' placeholder='Enter your password'/>
                     <img src={isPasswordVisible ? Eye : EyeOff} alt='eye' className='absolute right-3 top-2' style={{height: '24px', width: '24px', cursor: 'pointer'}} onClick={togglePasswordVisibility}/>
                     </div>
-                    <div className='mt-2 text-sm text-right text-purple-300'>
-                        <Link to="/forgot">Forgot Password</Link>
+                    <div className='mt-2 ml-auto text-sm text-left text-purple-300'>
+                        <Link to="/forgot">Forgot your Password?</Link>
                     </div>
                 </div>
                 <button type='submit' className='bg-secondary text-white font-inter rounded-full px-4 py-2 mt-5 font-semibold hover:bg-gray-500' style={{height: '40px', width: '280px'}} onClick={handleLogin}>Login</button>
@@ -57,3 +57,4 @@ const LoginScreen = () => {
 };
 
 export default LoginScreen;
+

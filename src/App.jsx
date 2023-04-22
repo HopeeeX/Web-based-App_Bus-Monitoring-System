8c7665ae614991a8ae354f927c1b0f381eb8832f
ChoiMillie <christinemedalla01@gmail.com>
2023-04-22 17:11:20 +0800

    Added Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import ForgotPassword from './pages/common/ForgotPassword';
-import LoginPage from './pages/common/LoginPage';
+import LoginScreen from './pages/common/LoginScreen';
 import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return(
     <Routes>
-      <Route path='/' element={<LoginPage/>}/>
+      <Route path='/' element={<LoginScreen/>}/>
       <Route path='/forgot' element={<ForgotPassword/>}/>
     </Routes>
   )

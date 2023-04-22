fda63ea00d41d8f1b45a2e6c8a46c611763032a1
JosuX <joferusa43@gmail.com>
2023-04-22 21:02:15 +0800

    Firebase Deployment Implemented

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyA8rHrFZDliux9cjEPtDronqrbe6Xu9YWY",
+  authDomain: "ube-express.firebaseapp.com",
+  databaseURL: "https://ube-express-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ube-express",
+  storageBucket: "ube-express.appspot.com",
+  messagingSenderId: "180420907476",
+  appId: "1:180420907476:web:cdd28dcc047fd9a7680641"
+};
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

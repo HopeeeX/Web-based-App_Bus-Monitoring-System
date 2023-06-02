273890612d74184a744db6c3d073caa90a9c1541
ChoiMillie <christinemedalla01@gmail.com>
2023-06-02 21:24:46 +0800

    Added Link in Row User

--- src/components/Table/RowUser.jsx
+++ src/components/Table/RowUser.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 
 const RowUser = ({text}) => {
     return (
         <tr>
-            {text.map((row, index) => (
-                    <td key={index}className='p-3 text-sm text-gray-600 font-inter font-semibold text-center'>{row}</td>))}
+            {text.map((row, index) => {
+                if(row.includes("link=")){
+                    row = row.replace("link=","");
+                    return (<td key={index}className='p-3 text-sm text-gray-600 font-inter font-semibold text-center'>
+                        <Link className='underline text-blue-400' to="/mechanic/viewInspection">{row}</Link>
+                        
+                    </td>)
+                }
+                else{
+                    return (<td key={index}className='p-3 text-sm text-gray-600 font-inter font-semibold text-center'>{row}</td>)
+                }
+            
+
+            }
+                    
+                    
+                    )}
         </tr>
     );
 }

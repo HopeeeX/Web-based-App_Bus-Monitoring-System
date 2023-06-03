20232f7ff359f05e223bb29d6847d94f515a17c6
JosuX <joferusa43@gmail.com>
2023-06-03 23:50:15 +0800

    Page info Update

--- src/pages/passenger/MainPageScreen.jsx
+++ src/pages/passenger/MainPageScreen.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import PassengerNavBar from './PassengerComponents/PassengerNavBar'
-import PassengerHero from './PassengerComponents/PassengerHero'
-import PassengerBusLocation from './PassengerComponents/PassengerBusLocation'
-import PassengerRating from './PassengerComponents/PassengerRating'
-import PassengerFooter from './PassengerComponents/PassengerFooter'
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import PassengerNavBar from './PassengerComponents/PassengerNavBar';
+import PassengerHero from './PassengerComponents/PassengerHero';
+import PassengerBusLocation from './PassengerComponents/PassengerBusLocation';
+import PassengerRating from './PassengerComponents/PassengerRating';
+import PassengerFooter from './PassengerComponents/PassengerFooter';
+import { useParams } from 'react-router-dom';
+import { firestore } from '../../../firebase';
+import { collection, doc, getDoc } from 'firebase/firestore';
+
 const MainPageScreen = () => {
-  const {busID} = useParams();
-  console.log(busID);
+  const { busID } = useParams();
+  const [busData, setBusData] = useState(null);
+
+  useEffect(() => {
+    const fetchBusData = async () => {
+      try {
+        const busDocRef = doc(firestore, 'buses', busID);
+        const busSnapshot = await getDoc(busDocRef);
+        if (busSnapshot.exists()) {
+          console.log(busSnapshot.data());
+          setBusData(busSnapshot.data());
+        } else {
+          console.log('Bus document not found');
+        }
+      } catch (error) {
+        console.error('Error fetching bus data:', error);
+      }
+    };
+
+    fetchBusData();
+  }, [busID]);
+
   return (
     <div className='w-full flex flex-col'>
-        <PassengerNavBar/>
-        <PassengerHero/>
-        <PassengerBusLocation/>
-        <PassengerRating/>
-        <PassengerFooter/>
+      <PassengerNavBar />
+      <PassengerHero />
+      <PassengerBusLocation busID={busID} busData={busData} />
+      <PassengerRating />
+      <PassengerFooter />
     </div>
-    
-  )
-}
+  );
+};
 
-export default MainPageScreen
+export default MainPageScreen;

652688a249233eabfea3357394115a126946b622
ChoiMillie <christinemedalla01@gmail.com>
2023-06-04 18:53:02 +0800

    Change icon

---
+++ src/components/Table/DeleteButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import tw from 'tailwind-styled-components'
+import Delete from '../../assets/icons/Delete.png'
+
+const Wrapper = tw.div`
+    mt-2.5`;
+
+const DeleteButton = () => {
+    return (
+    <Wrapper>
+        <button className='border outline-none'>
+            <img src={Delete} alt='option' className='w-[22px] h-[22px]'/>
+        </button>
+    </Wrapper>
+    );
+}
+
+export default DeleteButton;

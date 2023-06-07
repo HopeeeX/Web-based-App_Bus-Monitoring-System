7852779a97897a52600e3a03dac7f2a6001fee70
JosuX <joferusa43@gmail.com>
2023-06-08 04:13:04 +0800

    Inspection Form Validation

--- src/pages/driver/InspectionChecklist.jsx
+++ src/pages/driver/InspectionChecklist.jsx
@@ -1,58 +1,96 @@
-/* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState } from 'react';
 import InspectionFieldInput from './inspectionLists/InspectionFieldInput';
 import InspectionFront from './inspectionLists/InspectionFront';
 import InspectionRight from './inspectionLists/InspectionRight';
 import InspectionBack from './inspectionLists/InspectionBack';
 import InspectionLeft from './inspectionLists/InspectionLeft';
-import InspectionInside from './inspectionLists/InspectionInside'
+import InspectionInside from './inspectionLists/InspectionInside';
 import tw from 'tailwind-styled-components';
 import { useNavigate } from 'react-router-dom';
 import { InspectionAccess } from './inspectionLists/InspectionContext';
+import { firestore } from '../../../firebase';
+import { doc, getDoc } from "firebase/firestore";
 
 const InspectionChecklist = () => {
-  const {damaged} = InspectionAccess();
+  const { damaged } = InspectionAccess();
+  const [busIdError, setBusIdError] = useState('');
+  const [busId, setBusId] = useState('');
+  console.log(busIdError)
+
+  const handleBusIdChange = (e) => {
+    setBusId(e.target.value);
+    validateBusId(e.target.value);
+  };
 
   const submitReport = () => {
-    if(damaged.length === 0) {
-      //TODO Dispatch
+    if (damaged.length === 0) {
+      //TODO: Add Inspection Report
+      // TODO: Dispatch
     } else {
-      //TODO Submit Damaged Bus
+      const remarksInput = document.getElementById('remarksInput');
+      if (!remarksInput.value.trim()) {
+        alert('Remarks is required.');
+        return;
+      }
+      // TODO: Submit Damaged Bus
     }
-    navigate("/driver")
-  }
-
+    navigate('/driver');
+  };
+  
 
   const navigate = useNavigate();
 
-  
+  const validateBusId = async (busId) => {
+    try {
+      const docRef = doc(firestore, "buses", busId);
+      const docSnapshot = await getDoc(docRef);
 
+      if (!docSnapshot.exists()) {
+        setBusIdError('Bus ID does not exist.');
+      } else {
+        setBusIdError('');
+      }
+    } catch (error) {
+      console.error('Error validating bus ID:', error);
+    }
+  };
 
   const MainWrapper = tw.div`
-    flex flex-col justify-center items-center font-inter bg-[#F3F3F3] pb-14 md:pb-40 lg:pb-52 
-  `
+    flex flex-col justify-center items-center font-inter bg-[#F3F3F3] pb-14 md:pb-40 lg:pb-52
+  `;
   return (
     <MainWrapper>
-      <h1 className='w-full font-semibold text-center text-inspectionTitle  text-2xl sm:text-3xl md:text-4xl lg:text-[40px] border-b-2 pt-8 pb-6 border-secondary mb-8'>360-Degree Inspection Form</h1>
-      <InspectionFieldInput/>
+      <h1 className='w-full font-semibold text-center text-inspectionTitle  text-2xl sm:text-3xl md:text-4xl lg:text-[40px] border-b-2 pt-8 pb-6 border-secondary mb-8'>
+        360-Degree Inspection Form
+      </h1>
+      <InspectionFieldInput busIdError={busIdError} handleBusIdChange={handleBusIdChange} busId={busId}/>
       <div className='flex flex-col items-center gap-5 lg:gap-12'>
-        <InspectionFront/>
-        <InspectionRight/>
-        <InspectionBack/>
-        <InspectionLeft/>
-        <div className='flex flex-col gap-8 items-center'>
-          <InspectionInside/>
+        <InspectionFront />
+        <InspectionRight />
+        <InspectionBack />
+        <InspectionLeft />
+        <div className="flex flex-col gap-8 items-center">
+          <InspectionInside />
           <div className="remarksContainer">
-            <label htmlFor="remarksInput" className='text-base sm:text-lg md:text-xl lg:text-2xl'>Remarks</label>
-            <input type="text" id="remarksInput" placeholder="Enter your remarks" className='remarkInputs focus:outline-none focus:border-fontColor w-full' required/>
+            <label htmlFor="remarksInput" className="text-base sm:text-lg md:text-xl lg:text-2xl">
+              Remarks
+            </label>
+            <input
+              type="text"
+              id="remarksInput"
+              placeholder="Enter your remarks"
+              className="remarkInputs focus:outline-none focus:border-fontColor w-full"
+              required
+            />
           </div>
-          <button className='border outline-none buttonTemplate self-stretch' onClick={submitReport}>SUBMIT</button>
+          <button className="border outline-none buttonTemplate self-stretch" onClick={submitReport}>
+            SUBMIT
+          </button>
         </div>
       </div>
+      {busIdError && <p className="text-red-500">{busIdError}</p>}
     </MainWrapper>
-  )
-}
+  );
+};
 
-
-
-export default InspectionChecklist 
+export default InspectionChecklist;
--- src/pages/driver/inspectionLists/InspectionFieldInput.jsx
+++ src/pages/driver/inspectionLists/InspectionFieldInput.jsx
@@ -1,34 +1,61 @@
-import React from 'react'
-import { useEffect } from 'react';
+/* eslint-disable react/prop-types */
+import React, { useEffect, useRef} from 'react';
 
-const InspectionFieldInput = () => {
+const InspectionFieldInput = ({ busIdError, handleBusIdChange, busId }) => {
+  const busIdInputRef = useRef(null); // Create a ref for the busId input field
+
   useEffect(() => {
     var date = new Date();
-    document.getElementById("dateInput").value = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
-    .toISOString()
-    .split("T")[0];
-    document.getElementById("timeInput").value = date.toTimeString().slice(0,8)
+    document.getElementById('dateInput').value = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60000
+    )
+      .toISOString()
+      .split('T')[0];
+    document.getElementById('timeInput').value = date.toTimeString().slice(0, 8);
+
+    busIdInputRef.current.focus(); // Set focus on the busId input field
   }, []);
-  
 
   return (
     <div className="flex flex-col md:flex-row  justify-between w-full px-4 md:px-16 xl:px-32 font-inter font-semibold text-fontColor text-sm md:text-base mb-7 md:mb-11 lg:mb-16">
-      <div className="flex flex-col md:mb-3 ">
+      <div className="flex flex-col md:mb-3">
         <label htmlFor="busIdInput">Bus ID</label>
-        <input type="text" id="busIdInput" placeholder="Enter Number" className='inspectionInputs w-full md:w-48 lg:w-64 xl:w-80 focus:outline-none focus:border-fontColor' required/>
+        <input
+          type="text"
+          id="busIdInput"
+          placeholder="Enter Number"
+          className="inspectionInputs w-full md:w-48 lg:w-64 xl:w-80 focus:outline-none focus:border-fontColor"
+          required
+          value={busId}
+          onChange={handleBusIdChange}
+          ref={busIdInputRef} // Assign the ref to the input field
+        />
+        {busIdError && <span className="text-red-500">{busIdError}</span>}
       </div>
-      <div className='flex flex-col md:flex-row md:gap-5'>
-      <div className="flex flex-col md:mb-3">
-        <label htmlFor="dateInput">Date</label>
-        <input type="date" id="dateInput" className='inspectionInputs' readOnly="readonly" required />
-      </div>
-      <div className="flex flex-col md:mb-3">
-        <label htmlFor="timeInput">Time</label>
-        <input type="time" id="timeInput" className='inspectionInputs' readOnly="readonly" required />
-      </div>
+      <div className="flex flex-col md:flex-row md:gap-5">
+        <div className="flex flex-col md:mb-3">
+          <label htmlFor="dateInput">Date</label>
+          <input
+            type="date"
+            id="dateInput"
+            className="inspectionInputs"
+            readOnly="readonly"
+            required
+          />
+        </div>
+        <div className="flex flex-col md:mb-3">
+          <label htmlFor="timeInput">Time</label>
+          <input
+            type="time"
+            id="timeInput"
+            className="inspectionInputs"
+            readOnly="readonly"
+            required
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InspectionFieldInput
+export default InspectionFieldInput;

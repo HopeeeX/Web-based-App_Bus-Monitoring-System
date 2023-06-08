b35d7a1b822b96e5ebb42d95846b8e0ddb5cf1ff
ChoiMillie <christinemedalla01@gmail.com>
2023-06-08 20:43:53 +0800

    Edit Status Component

--- src/components/Table/Status.jsx
+++ src/components/Table/Status.jsx
@@ -1,22 +1,37 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
 const getStatusBackgroundColor = (status) => {
-  if (status === 'Pending') {
-    return 'bg-[#FBFB79E0]';
-  } else if (status === 'Approved') {
-    return 'bg-[#77DD77E0]';
-  } else if (status === 'Unapproved') {
-    return 'bg-[#FF6961E0]';
-  }
+    if (status === 'Pending') {
+        return 'bg-[#FBFB79E0] text-[#A7A9AC]';
+    } else if (status === 'Approved') {
+        return 'bg-[#77DD77E0] text-white';
+    } else if (status === 'Unapproved') {
+        return 'bg-[#FF6961E0] text-white';
+    }
 };
 
-const Status = ({ status }) => {
-  return (
-    <span className={`rounded-full px-3 py-1 text-sm font-medium ${getStatusBackgroundColor(status)}`}>
-      {status}
-    </span>
-  );
+const Status = ({ status}) => {
+    const [editableStatus, setEditableStatus] = useState(status);
+
+    const handleStatusChange = (event) => {
+    setEditableStatus(event.target.value);
+};
+
+
+return (
+    <div className='inline-block'>
+        <select
+        className={`rounded-full px-3 py-1 text-sm font-inter font-semibold border outline-none -ml-4 ${getStatusBackgroundColor(editableStatus)}`}
+        value={editableStatus}
+        onChange={handleStatusChange}
+        >
+        <option value="Pending" className='bg-white text-[#A7A9AC]'>Pending</option>
+        <option value="Approved" className='bg-white text-[#A7A9AC]'>Approved</option>
+        <option value="Unapproved" className='bg-white text-[#A7A9AC]'>Unapproved</option>
+        </select>
+    </div>
+    );
 };
 
 export default Status;
